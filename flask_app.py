d7a5dd34fcc125aee4d19fa22befef90068e65f3
krpopkin <krpopkin@gmail.com>
2021-04-17 11:20:22 -0400

    Promote latest changes
    Promote latest changes

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello from us to them!'
 
 
 if __name__ == '__main__':
