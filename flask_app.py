8d2b5c506fc396b1c3aaf3c3604d19ab34ebfd59
krpopkin <krpopkin@gmail.com>
2021-07-17 17:26:02 -0400

    Updates
    updates

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,18 @@
+##############################################################################################################################
+# Render the run status reports (index.html) form, populating the "select project" dropdown with the Trello boards the user
+# has access to.  Then submit the selected project or "all projects" back to the Trello App to produce the status reports.
+##############################################################################################################################
+
+# Helpful links:
+# https://csveda.com/fill-a-dropdown-combo-in-flask-and-postgresql/
+
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-
 # default page that will be used for additional development
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
